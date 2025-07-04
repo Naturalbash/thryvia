a4457e84b232bbfa81a29b974a40651c70e24e31
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-07-04 13:16:58 +0100

    fix: ensure sign-in button is disabled during form submission

--- src/components/sign-in/sign-in-btn.tsx
+++ src/components/sign-in/sign-in-btn.tsx
@@ -7,6 +7,7 @@ export function SignInButton() {
   const { pending } = useFormStatus();
   return (
     <button
+      disabled={pending}
       type="submit"
       className="w-full flex justify-center items-center py-2 bg-slate-700 text-white font-semibold border-none rounded-sm text-md cursor-pointer hover:bg-slate-600 transition"
     >
